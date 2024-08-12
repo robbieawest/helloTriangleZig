a9afb5d9413a24b25b704a8009f70f281f24bd35
Robbie West <robbie.west@ventriks.com>
2024-08-12 23:12:32 +0100

    Hello Triangle! opengl colours aren't working..

--- src/main.zig
+++ src/main.zig
@@ -45,7 +45,7 @@ var procs: gl.ProcTable = undefined;
 
 pub fn main() !void {
     std.debug.print("Hello world!", .{});
-    var window = try sf.RenderWindow.createDefault(.{ .x = 800, .y = 800 }, "SFML works!");
+    var window = try sf.RenderWindow.createDefault(.{ .x = 800, .y = 800 }, "Hello Triangle");
     defer window.destroy();
 
     if (!procs.init(getProcAddress)) return error.InitFailed;
@@ -53,13 +53,66 @@ pub fn main() !void {
     gl.makeProcTableCurrent(&procs);
     defer gl.makeProcTableCurrent(null);
 
+    const vertices = [9]f32{ -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0 }; //stupid zls formatting gonna make me jump off a high structure
+
+    var VAO: c_uint = undefined;
+    gl.GenVertexArrays(1, @ptrCast(&VAO));
+    gl.BindVertexArray(VAO);
+
+    var VBO: c_uint = undefined;
+    gl.GenBuffers(1, @ptrCast(&VBO));
+    defer gl.DeleteBuffers(1, @ptrCast(&VBO));
+    gl.BindBuffer(gl.ARRAY_BUFFER, VBO);
+    gl.BufferData(gl.ARRAY_BUFFER, @sizeOf(f32) * vertices.len, &vertices, gl.STATIC_DRAW);
+
+    const vertexShaderSource = "#version 330core\n" ++
+        "layout (location = 0) in vec3 aPos;\n" ++
+        "void main()\n" ++
+        "{\n" ++
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" ++
+        "};";
+
+    const vertexShader: c_uint = gl.CreateShader(gl.VERTEX_SHADER);
+    defer gl.DeleteShader(vertexShader);
+    gl.ShaderSource(vertexShader, 1, &[1][*]const u8{vertexShaderSource}, null);
+    gl.CompileShader(vertexShader);
+
+    const fragmentShaderSource = "#version 330core\n" ++
+        "out vec4 FragColor;\n" ++
+        "void main()\n" ++
+        "{\n" ++
+        "   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n" ++
+        "};";
+
+    const fragmentShader: c_uint = gl.CreateShader(gl.FRAGMENT_SHADER);
+    defer gl.DeleteShader(fragmentShader);
+    gl.ShaderSource(fragmentShader, 1, &[1][*]const u8{fragmentShaderSource}, null);
+    gl.CompileShader(fragmentShader);
+
+    const shaderProgram: c_uint = gl.CreateProgram();
+    gl.AttachShader(shaderProgram, vertexShader);
+    gl.AttachShader(shaderProgram, fragmentShader);
+    gl.LinkProgram(shaderProgram);
+    gl.UseProgram(shaderProgram);
+
+    //Linking vertex attributes, simple 3 v pos no colour or normals tight packed
+    gl.VertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 3 * @sizeOf(f32), 0);
+    gl.EnableVertexAttribArray(0);
+
     while (window.isOpen()) {
-        std.debug.print("In loop\n", .{});
         while (window.pollEvent()) |event| {
             if (event == .closed)
                 window.close();
         }
 
-        gl.Clear(gl.COLOR_BUFFER_BIT);
+        gl.ClearColor(0.2, 0.5, 0.3, 1.0);
+        window.clear(sf.Color.Blue);
+
+        //Draw
+        gl.UseProgram(shaderProgram);
+        gl.BindVertexArray(VAO);
+
+        gl.DrawArrays(gl.TRIANGLES, 0, 3);
+        window.display();
     }
 }
